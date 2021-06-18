b3528058966ebaf7a31fe1c4f30a5cbf93bfcbab
unknown <you@example.com>
2021-06-18 18:56:26 +0500

    class16

---
+++ app.js
@@ -0,0 +1,57 @@
+// function readMore(){
+//     var text = "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Eaque esse eius quam vero, deleniti distinctio! Deleniti nobismagni qui quisquam nesciunt commodi odio quidem,ab omnis suscipit, molestiae reiciendis sunt?</"
+//     var paragraf = document.getElementById('para')
+//     paragraf.innerHTML=text
+// }
+// function readMores(){
+//     var text = document.getElementById('text').innerHTML;
+//     var paragraf = document.getElementById('para')
+//     paragraf.innerHTML=text
+// }
+
+// function big (){
+//     var img = document.getElementById("car")
+//     img.className = "car"
+// }
+
+// function big (){
+//     var img = document.getElementById("car")
+//     img.className += " car"
+// }
+
+// function imageChange(){
+//     var carimg = document.getElementById("car")
+//     carimg.src= "download.jpg"
+// }
+
+// function imageChangeBefore(){
+//     var carimg = document.getElementById("car")
+//     carimg.src= "https://images.pexels.com/photos/241316/pexels-photo-241316.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+// }
+
+// function imageChange(){
+//     var carimg = document.getElementById("car");
+//     carimg.style.width = "50%"
+// }
+
+// function imageChange(){
+//     var carimg = document.getElementById("para");
+//     carimg.style.fontSize = "3em"
+// }
+
+
+    // var para = document.getElementsByTagName("p");
+    // para[3].style.color = "red"
+
+    // for(var i =0 ; i<para.length; i++){
+    //     para[i].style.color="blue"
+    // }
+
+    var parent =document.getElementById("parent")
+    var para = parent.getElementsByTagName("p");
+    // para[3].style.color = "red"
+
+    for(var i =0 ; i<para.length; i++){
+        para[i].style.color="blue"
+    }
+
